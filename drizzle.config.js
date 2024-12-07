ebe0a97b2fef024758d42f7282ed35c96c97956f
AshKemp <kmuraliashwin@gmail.com>
2024-12-07 15:36:50 +0530

    1. install drizzle 2. create drizzle schema 3. create db config

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+export default {
+  dialect: "postgresql",
+  schema: "./utils/schema.jsx",
+  out: "./drizzle",
+  dbCredentials: {
+    url: "postgresql://finan-smart_owner:nb89dLJSXReg@ep-noisy-poetry-a5pgswop.us-east-2.aws.neon.tech/finan-smart?sslmode=require",
+    connectionStrings:
+      "postgresql://finan-smart_owner:nb89dLJSXReg@ep-noisy-poetry-a5pgswop.us-east-2.aws.neon.tech/finan-smart?sslmode=require",
+  },
+};
